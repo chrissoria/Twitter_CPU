3115aba7ce311c2e3c755f32d5006c0091d95dd6
chrissoria <csoria1@mail.ccsf.edu>
2016-02-12 19:36:40 -0800

    API set up

--- Twitter_CPU/TwitterClient.swift
+++ Twitter_CPU/TwitterClient.swift
@@ -9,6 +9,16 @@
 import UIKit
 import BDBOAuth1Manager
 
-class TwitterClient: BDBOAuth1RequestOperationManager {
+let twitterConsumerKey = "Blfsv7a1evn8F0bAIcTKB5Hvu"
+let twitterConsumerSecret = "Dhr0k74zfaRQsGWvtBNPNLGR487JWoRz5eBaWHbRsireftpVgr"
+let twitterBaseURL = NSURL(string: "https://api.twitter.com")
 
+class TwitterClient: BDBOAuth1RequestOperationManager {
+    
+    class var sharedInstance: TwitterClient{
+        struct Static{
+            static let instance = TwitterClient(baseURL: twitterBaseURL, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
+        }
+        return Static.instance
+    }
 }
