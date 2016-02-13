280f8e1c4a33fa7ef4b0e6c18a245ecf0107c3a6
chrissoria <csoria1@mail.ccsf.edu>
2016-02-12 19:14:51 -0800

    TwitterClient

---
+++ Twitter_CPU/TwitterClient.swift
@@ -0,0 +1,14 @@
+//
+//  TwitterClient.swift
+//  Twitter_CPU
+//
+//  Created by christopher soria on 2/12/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class TwitterClient: BDBOAuth1RequestOperationManager {
+
+}
